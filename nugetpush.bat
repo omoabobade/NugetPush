725fa642910143d7f53c9dd3a5edb223cb4ac39b
kolawole abobade <kolawole@flutterwavego.com>
2020-11-27 02:18:25 +0000

    first commit

---
+++ nugetpush.bat
@@ -0,0 +1,26 @@
+@echo off
+
+setlocal
+if "%1%"=="--v" ( 
+echo nugetpush version 1.0.0
+echo By Kolawole Abobade kolawole.abobade@outlook.com
+) else ( 
+	echo ::::::::Packaging your library ::::::::
+	call dopackage.bat
+	echo %ERRORLEVEL%
+	IF %ERRORLEVEL% NEQ 0 (
+		echo "An error occur while packing file"
+		goto:eof 
+	)
+	echo ::::::::Fetching secret key...::::::::
+	set /p secretkey=<%1%
+	echo "Fetched secret key"
+	@set "dr=%CD%\ *.nupkg"
+	for %%a in (*.nupkg) do set nupkg=%%a
+	echo ::::::::Package found and about to push to nuget::::::::
+	nuget push %nupkg% %secretkey% -Source https://api.nuget.org/v3/index.json"
+
+	pause
+
+)
+endlocal
